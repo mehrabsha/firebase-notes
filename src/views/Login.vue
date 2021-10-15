4c12e678068d6082b716b4a15da8dd63150028f2
mehrab <mehrab572@gmail.com>
2021-10-15 14:11:06 +0330

    add login page (ui)

---
+++ src/views/Login.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <v-row align="center" justify="center" no-gutters style="height: 100vh;">
+      <v-col cols="10" md="3">
+        <v-card elevation="2" class="pa-9" style="height : fit-content">
+          <h2 class="text-center">LOGIN</h2>
+          <v-text-field
+            class="pt-10 pb-7"
+            label="E-mail"
+            outlined
+          ></v-text-field>
+          <v-btn
+            elevation="2"
+            color="primary"
+            class="mt-16 ml-auto mr-0 d-block"
+            >LOGIN</v-btn
+          >
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
+</template>
